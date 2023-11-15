e23e3829a17814c0e6b72e604fc191f6146717f6
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-11-15 21:28:49 +0530

    updations

--- harmonyfrontend/harmony/src/app/app.component.ts
+++ harmonyfrontend/harmony/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'harmony';
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
 }
